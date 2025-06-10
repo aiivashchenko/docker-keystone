e4c1a43950b43359deda58c18df455850820c108
Andrei Ivashchenko <aiivashchenko@users.noreply.github.com>
2025-06-10 16:58:43 +0200

    update keystone

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,20 @@
-FROM python:2.7
-MAINTAINER krystism "krystism@gmail.com"
+FROM ubuntu:22.04
 
-ENV VERSION=10.0.0.0rc1
-
-RUN set -x \
-    && apt-get -y update \
-    && apt-get install -y libffi-dev python-dev libssl-dev mysql-client python-mysqldb \
-    && apt-get -y clean
-
-RUN curl -fSL https://github.com/openstack/keystone/archive/${VERSION}.tar.gz -o keystone-${VERSION}.tar.gz \
-    && tar xvf keystone-${VERSION}.tar.gz \
-    && cd keystone-${VERSION} \
-    && pip install -r requirements.txt \
-    && PBR_VERSION=${VERSION}  pip install . \
-    && pip install uwsgi MySQL-python \
-    && cp -r etc /etc/keystone \
-    && pip install python-openstackclient \
-    && cd - \
-    && rm -rf keystone-${VERSION}*
+RUN apt-get -y update && \
+    apt-get install -y --no-install-recommends memcached mariadb-server python3-dev python3-pip python3-setuptools git-core gcc
+RUN git clone --branch 27.0.0 --single-branch --depth 1 https://github.com/openstack/keystone.git /usr/local/src/keystone && \
+    pip install --upgrade setuptools pymysql uwsgi && \
+    cd /usr/local/src/keystone && python3 setup.py develop
+RUN apt-get remove -y --purge git-core git gcc && \
+    apt-get autoremove -y --purge && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
 
 COPY keystone.conf /etc/keystone/keystone.conf
-COPY keystone.sql /root/keystone.sql
+COPY keystone.sql /keystone.sql
 
-# Add bootstrap script and make it executable
-COPY bootstrap.sh /etc/bootstrap.sh
-RUN chown root:root /etc/bootstrap.sh && chmod a+x /etc/bootstrap.sh
+COPY bootstrap.sh /bootstrap.sh
+RUN chmod a+x /bootstrap.sh
 
-ENTRYPOINT ["/etc/bootstrap.sh"]
 EXPOSE 5000 35357
-
-HEALTHCHECK --interval=10s --timeout=5s \
-  CMD curl -f http://localhost:5000/v3 2> /dev/null || exit 1; \
-  curl -f http://localhost:35357/v3 2> /dev/null || exit 1; \
+ENTRYPOINT ["/bootstrap.sh"]
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,75 +1,43 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -x
 
-# Init the arguments
-ADMIN_TOKEN=${ADMIN_TOKEN:-294a4c8a8a475f9b9836}
-ADMIN_TENANT_NAME=${ADMIN_TENANT_NAME:-admin}
-ADMIN_USER_NAME=${ADMIN_USERNAME:-admin}
-ADMIN_PASSWORD=${ADMIN_PASSWORD:-ADMIN_PASS}
-ADMIN_EMAIL=${ADMIN_EMAIL:-${ADMIN_USER_NAME}@example.com}
+/usr/sbin/service mariadb start
+/usr/sbin/service memcached start
 
-OS_TOKEN=$ADMIN_TOKEN
-OS_URL=${OS_AUTH_URL:-"http://${HOSTNAME}:35357/v3"}
-OS_IDENTITY_API_VERSION=3
-
-CONFIG_FILE=/etc/keystone/keystone.conf
-SQL_SCRIPT=${SQL_SCRIPT:-/root/keystone.sql}
-
-if env | grep -qi MYSQL_ROOT_PASSWORD && test -e $SQL_SCRIPT; then
-    MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-$MYSQL_ENV_MYSQL_ROOT_PASSWORD}
-    MYSQL_HOST=${MYSQL_HOST:-mysql}
-    sed -i "s#^connection.*=.*#connection = mysql://keystone:KEYSTONE_DBPASS@${MYSQL_HOST}/keystone#" $CONFIG_FILE
-    mysql -uroot -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST <$SQL_SCRIPT
+addgroup --system keystone > /dev/null || true
+adduser --quiet --system --no-create-home --ingroup keystone --shell /bin/false keystone || true
+if [ "$(id -gn keystone)"  = "nogroup" ]
+then
+    usermod -g keystone keystone
 fi
 
-rm -f $SQL_SCRIPT
+mkdir -p /var/lib/keystone/ /etc/keystone/ /var/log/keystone/
+chown keystone:keystone -R /var/lib/keystone/ /etc/keystone/ /var/log/keystone/
+chmod 0700 /var/lib/keystone/ /var/log/keystone/ /etc/keystone/
 
-# update keystone.conf
-sed -i "s#^admin_token.*=.*#admin_token = $ADMIN_TOKEN#" $CONFIG_FILE
+mysql < /keystone.sql
 
-# Populate the Identity service database
 keystone-manage db_sync
-# Initialize Fernet keys
-keystone-manage fernet_setup --keystone-user root --keystone-group root
-mv /etc/keystone/default_catalog.templates /etc/keystone/default_catalog
+keystone-manage db_sync --expand
+keystone-manage db_sync --check
+keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+keystone-manage bootstrap \
+    --bootstrap-password ${ADMIN_PASSWORD} \
+    --bootstrap-username ${ADMIN_USERNAME} \
+    --bootstrap-project-name ${ADMIN_TENANT_NAME} \
+    --bootstrap-role-name admin \
+    --bootstrap-service-name keystone \
+    --bootstrap-region-id RegionOne \
+    --bootstrap-admin-url http://${HOSTNAME}:35357 \
+    --bootstrap-public-url http://${HOSTNAME}:5000 \
+    --bootstrap-internal-url http://${HOSTNAME}:5000
 
-# start keystone service 
-uwsgi --http 0.0.0.0:35357 --wsgi-file $(which keystone-wsgi-admin) &
-# uwsgi --http 0.0.0.0:5000 --wsgi-file $(which keystone-wsgi-public) &
-sleep 5 # wait for start
+oslopolicy-policy-generator --namespace keystone --output-file /etc/keystone/policy.yaml
+oslopolicy-policy-upgrade --namespace keystone --policy /etc/keystone/policy.yaml --output-file /etc/keystone/policy.yaml
 
-export OS_TOKEN OS_URL OS_IDENTITY_API_VERSION
-
-# Initialize account
-openstack service create  --name keystone identity
-openstack endpoint create --region RegionOne identity public http://${HOSTNAME}:5000/v3
-openstack endpoint create --region RegionOne identity internal http://${HOSTNAME}:5000/v3
-openstack endpoint create --region RegionOne identity admin http://${HOSTNAME}:5000/v3
-openstack domain create --description "Default Domain" default
-openstack project create --domain default  --description "Admin Project" admin
-openstack user create --domain default --password $ADMIN_PASSWORD admin
-openstack role create admin
-openstack role add --project admin --user admin admin
-
-unset OS_TOKEN OS_URL
-
-# Write openrc to disk
-cat >~/openrc <<EOF
-export OS_PROJECT_DOMAIN_NAME=default
-export OS_USER_DOMAIN_NAME=default
-export OS_PROJECT_NAME=admin
-export OS_USERNAME=admin
-export OS_PASSWORD=${ADMIN_PASSWORD}
-export OS_AUTH_URL=http://${HOSTNAME}:35357/v3
-export OS_IDENTITY_API_VERSION=3
-export OS_IMAGE_API_VERSION=2
-EOF
-
-cat ~/openrc
-
-# reboot services
-pkill uwsgi
-sleep 5
-uwsgi --http 0.0.0.0:5000 --wsgi-file $(which keystone-wsgi-public) &
-sleep 5
-uwsgi --http 0.0.0.0:35357 --wsgi-file $(which keystone-wsgi-admin)
+uwsgi --http 0.0.0.0:5000 --threads 2 --uid keystone --wsgi-file $(which keystone-wsgi-public) &
+P1=$!
+uwsgi --http 0.0.0.0:35357 --threads 2 --uid keystone --wsgi-file $(which keystone-wsgi-admin) &
+P2=$!
+wait ${P1} ${P2}
